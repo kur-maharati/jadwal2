0f9035b4d0d1df86e847de432ec0f0003b8349f3
kur-maharati <kur.maharati@gmail.com>
2026-08-03 09:37:21 +0700

    Add configuration settings for API and school name

--- config.js
+++ config.js
@@ -1,1 +1,9 @@
+const CONFIG = {
 
+    API_URL: "https://script.google.com/macros/s/AKfycbxLwvWKkjTjrU_AQeGv9W8vfrMexV-O9xERe43eB_7V4Y0aoS06ZAmTb-ggiApBliRyQQ/exec",
+
+    AUTO_REFRESH: 30000,
+
+    SCHOOL_NAME: "SMK MAHARATI"
+
+};
