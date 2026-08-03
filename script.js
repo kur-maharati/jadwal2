2b3b9edf9ab3552d9e015f55992489f9242a6345
kur-maharati <kur.maharati@gmail.com>
2026-08-03 09:39:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+let DATA = {};
 
+async function loadData(){
+
+    try{
+
+        const response = await fetch(CONFIG.API_URL);
+
+        DATA = await response.json();
+
+        console.log(DATA);
+
+        document.getElementById("status").innerHTML =
+            "Data berhasil dimuat";
+
+    }
+
+    catch(err){
+
+        console.log(err);
+
+        document.getElementById("status").innerHTML =
+            "Tidak dapat mengambil data";
+
+    }
+
+}
+
+loadData();
